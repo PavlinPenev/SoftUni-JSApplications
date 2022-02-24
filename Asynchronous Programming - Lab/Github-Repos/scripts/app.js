74ec2cc89fedcf43d14b63afa09951b5f737cae2
Paf <pavlinpenevv@abv.bg>
2022-02-24 23:49:41 +0200

    Asynchronous Programming - Lab

--- Asynchronous Programming - Lab/Github-Repos/scripts/app.js
+++ Asynchronous Programming - Lab/Github-Repos/scripts/app.js
@@ -8,23 +8,34 @@ async function loadRepos() {
 
 	const endpoint = `https://api.github.com/users/${username}/repos`;
 
-	const repos = await fetch(endpoint);
+	let errorHandle = '';
 
-	const reposData = await repos.json();
-
-	reposData.forEach(repo => {
+	try {
 		
-		const fullName = repo.full_name;
-		const url = repo.html_url;
+		const repos = await fetch(endpoint);
+		errorHandle = `Status code: ${repos.status} ${repos.statusText}`;
+		const reposData = await repos.json();
+		reposData.forEach(repo => {
+			
+			const fullName = repo.full_name;
+			const url = repo.html_url;
+	
+			const liElement = document.createElement('li');
+			ulRepos.appendChild(liElement);
+	
+			const aHyperLinkRepo = document.createElement('a');
+			aHyperLinkRepo.href = `${url}`;
+			aHyperLinkRepo.textContent = `${fullName}`;
+			liElement.appendChild(aHyperLinkRepo);
+	
+		});
 
+	} catch {
+		
 		const liElement = document.createElement('li');
 		ulRepos.appendChild(liElement);
+		liElement.textContent = errorHandle;
 
-		const aHyperLinkRepo = document.createElement('a');
-		aHyperLinkRepo.href = `${url}`;
-		aHyperLinkRepo.textContent = `${fullName}`;
-		liElement.appendChild(aHyperLinkRepo);
-
-	});
+	}
 
 }
