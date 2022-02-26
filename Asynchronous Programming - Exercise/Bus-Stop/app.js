d32aa87a2b2320af3e93451a399d26a28517adc9
Paf <pavlinpenevv@abv.bg>
2022-02-26 17:57:21 +0200

    Asynchronous Programming - Exercise

---
+++ Asynchronous Programming - Exercise/Bus-Stop/app.js
@@ -0,0 +1,49 @@
+window.onload = getInfo;
+
+function getInfo() {
+
+    const buttonElement = document.getElementById('submit');
+    buttonElement.addEventListener('click', loadBusStopInfo);
+
+    const busIdElement = document.getElementById('stopId');
+
+    const ulElement = document.getElementById('buses');
+    const nameDivElement = document.getElementById('stopName');
+
+    async function loadBusStopInfo(ev) {
+        
+        ev.preventDefault();
+
+        if (busIdElement.value) {
+            
+            const busIdValue = busIdElement.value;
+
+            const endpoint = `http://localhost:3030/jsonstore/bus/businfo/${busIdValue}`;
+
+            ulElement.innerHTML = '';
+
+            try {
+
+                const busStopResponse = await fetch(endpoint);
+
+                const busStopResult = await busStopResponse.json();
+
+                nameDivElement.textContent = busStopResult.name;
+
+                for (const key in busStopResult.buses) {
+                
+                    const liElement = document.createElement('li');
+                    liElement.textContent = `Bus ${key} arrives in ${busStopResult.buses[key]} minutes`;
+                    ulElement.appendChild(liElement);
+
+                }
+
+            } catch {
+
+                nameDivElement.textContent = `Error`;
+                
+            }
+
+        }
+    }
+}
